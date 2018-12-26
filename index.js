0eeac8757a1f91d19f1b2502a8a59051069f81a4
manio4444 <kalinowskixmarcin@o2.pl>
2018-12-27 00:09:02 +0100

    json-stringify-safe added

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const server = express();
+const stringify = require('json-stringify-safe');
 
 server.get('/', (req, res) => {
-  res.send('helo word');
-  console.log('cached');
+  // res.send('helo word' + '<br>' + req);
+  res.send(stringify(req.headers  ));
+
+  console.log('cached - ' + req.headers.host);
   // console.log(req);
 });
 
-serverListerPort = 80;
+serverListerPort = 8088;
 
 server.listen (serverListerPort, () => {
   console.log(`Server started on port ${serverListerPort}`);
